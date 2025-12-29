0d3eba7e0a7c5cd3f6ed921d2b76255c1ee055af
GianpieroTulipani <50196821+GianpieroTulipani@users.noreply.github.com>
2025-12-29 14:37:10 +0100

    removed the print

--- cirkit/templates/learn_spn.py
+++ cirkit/templates/learn_spn.py
@@ -208,12 +208,11 @@ class LearnSPN:
         if num_input_units == 1:
             logits = np.log(probs_np).reshape(1, num_categories)
         else:
-            base = probs_np#np.log(probs_np)
+            base = np.log(probs_np)
             logits = np.tile(base.reshape(1, num_categories), (num_input_units, 1))
             if self.noise_scale and self.noise_scale > 0.0:
                 logits = logits + np.random.normal(loc=0.0, scale=self.noise_scale, size=logits.shape)
 
-        print(f'({num_input_units}, {num_categories}):{logits}')
         tp = TensorParameter(
             num_input_units,
             num_categories,
@@ -245,11 +244,9 @@ class LearnSPN:
             rep_weights_expandend = np.tile(rep_weights.reshape(num_sum_units, arity, 1), (1, 1, num_input_units))
             rep_weights_flat = rep_weights_expandend.reshape(num_sum_units, arity * num_input_units)
             
-            logits = rep_weights_flat#np.log(rep_weights_flat)
+            logits = np.log(rep_weights_flat)
             if self.noise_scale and self.noise_scale > 0.0:
                 logits = logits + np.random.normal(loc=0.0, scale=self.noise_scale, size=logits.shape)
-
-        print(f'({num_input_units}, {num_sum_units}):{logits}')
 
         tp = TensorParameter(
             num_sum_units,
